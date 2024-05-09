f219736ff6ab19dbf7f9f4856b0e75fc0cb13388
Sergio Zaldivar <55958806+zayser99@users.noreply.github.com>
2024-05-09 17:01:23 -0600

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: 'ti-admin/designs/', // Ruta base de la aplicación
+  build: {
+    outDir: 'dist', // Carpeta de salida para los archivos generados
+    assetsDir: 'assets', // Carpeta de activos (CSS, JS, imágenes, etc.)
+  },
+});
