865670e263741751bb236b76ccee64925a8b005a
Sergio Zaldivar <55958806+zayser99@users.noreply.github.com>
2024-05-10 09:07:05 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/reactvite/', // Ruta base de la aplicación
@@ -6,4 +7,6 @@ export default defineConfig({
     outDir: 'dist', // Carpeta de salida para los archivos generados
     assetsDir: 'assets', // Carpeta de activos (CSS, JS, imágenes, etc.)
   },
+  plugins: [react()],
 });
+
